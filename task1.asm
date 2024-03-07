7d3085ace1c8743200e679182ef2ab00d9eebab9
Nikitat0 <nikitatzero@yandex.ru>
2024-03-07 18:21:33 +0700

    task1: Tidy

--- task1.asm
+++ task1.asm
@@ -1,13 +1,12 @@
-.include "lib1.asm"
+.include "lib_core.asm"
 
 main:
 	readch
 	addi a0, a0, -48 # -'0'
 	sltiu a0, a0, 10
-    beq zero, a0, main_exit
+    beqz, a0, exit
     li a0, '1'
     printch
-	li a0, '\n'
-	printch
-main_exit:
-    exit
+
+exit:
+    exit 0
