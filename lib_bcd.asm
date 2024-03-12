1b1d679b65706123e46e3c6bc07d9b4de603884a
Nikitat0 <nikitatzero@yandex.ru>
2024-03-12 13:20:52 +0700

    task4: Tidy

---
+++ lib_bcd.asm
@@ -0,0 +1,68 @@
+.macro bcd_abs %rd, %rs
+    srli %rd, %rs, 4
+.end_macro
+
+.text
+from_bcd_digit: # int from_bcd_digit(int c)
+    addi a0, a0, -48 # -'0'
+    sltiu t0, a0, 10
+    beqz t0, from_bcd_digit_error
+    ret
+from_bcd_digit_error:
+    error "Invalid digit"
+
+normalize_bcd: # int normalize_bcd(int bcd)
+    srli t0, a0, 4
+    sltiu t0, t0, 1
+    not t0, t0
+    and a0, a0, t0
+    ret
+
+scan_bcd_number: # int scan_bcd_number()
+    push3 ra, s1, s2
+    li s1, 0
+    li s2, 0xa
+    readch
+    xori t0, a0, '-'
+    sltiu t0, t0, 1
+    add s2, s2, t0
+    beqz t0, scan_bcd_number_1
+    readch
+scan_bcd_number_1:
+    srli t0, s1, 24
+    andi t0, t0, 0xF
+    bnez t0, scan_bcd_number_error
+    slli s1, s1, 4
+    call from_bcd_digit
+    or s1, s1, a0
+    readch
+    xori t0, a0, 0xa # "\n"
+    bnez t0, scan_bcd_number_1
+    slli a0, s1, 4
+    or a0, a0, s2
+    call normalize_bcd
+    pop3 ra, s1, s2
+    ret
+scan_bcd_number_error:
+    error "Too big number"
+
+print_bcd_number: # void print_bcd_number(int bcd)
+    push3 ra, fp, s1
+    mv fp, sp
+    srli s1, a0, 4
+    andi a0, a0, 0x1
+    beqz a0, print_bcd_number_1
+    li a0, '-'
+    printch
+print_bcd_number_1:
+    andi a0, s1, 0xF
+    addi a0, a0, '0'
+    push a0
+    srli s1, s1, 4
+    bne zero, s1, print_bcd_number_1
+print_bcd_number_2:
+    pop a0
+    printch
+    bne sp, fp, print_bcd_number_2
+    pop3 ra, fp, s1
+    ret
--- task4.asm
+++ task4.asm
@@ -1,134 +1,108 @@
-.include "lib1.asm"
+.include "lib_core.asm"
+.include "lib_bcd.asm"
 
-.macro parse_digit
-    addi a0, a0, -48 # -'0'
-    sltiu t0, a0, 10
-    beq zero, t0, exit
-.end_macro
-
-.macro to_digit
-    addi a0, a0, '0'
-.end_macro
-
+.text
 main:
-    call read_number
+    call scan_bcd_number
     mv s1, a0
-    call read_number
+    call scan_bcd_number
     mv s2, a0
     readch
-    mv a2, a0
+    xori t0, a0, '+'
+    beqz, t0, main_add
+    xori t0, a0, '-'
+    beqz, t0, main_sub
+main_error:
+    error "Invalid operation"
+main_sub:
+    xori s2, s2, 1
+main_add:
     mv a0, s1
     mv a1, s2
-    xori t0, a2, '+'
-    beq zero, t0, main_add
-    xori t0, a2, '-'
-    beq zero, t0, main_sub
-exit:
-    exit
-main_add:
-    call add
-    call print_number
-    exit
-main_sub:
-    call sub
-    call print_number
-    exit
+    call bcd_add
+    call print_bcd_number
+    exit 0
 
-add:
+bcd_add_sub:
+    andi s1, a0, 1
+    bcd_abs t0, a0
+    bcd_abs t1, a1
+    slt t2, t0, t1
+    xor s1, s1, t2
+    neg t2, t2
+    xor a0, t0, t1
+    and a0, a0, t2
+    xor a1, a0, t1
+    xor a0, a0, t0
+    call ubcd_sub
+    slli a0, a0, 4
+    or a0, a0, s1
+    call normalize_bcd
+    j bcd_add_1
+bcd_add: # bcd_t bcd_add(bcd_t lhs, bcd_t rhs)
+    push2 ra, s1
+    xor t0, a0, a1
+    andi t0, t0, 1
+    bnez t0, bcd_add_sub
+    andi s1, a0, 1
+    srli a0, a0, 4
+    srli a1, a1, 4
+    call ubcd_add
+    slli a0, a0, 4
+    or a0, a0, s1
+bcd_add_1:
+    pop2 ra, s1
+    ret
+
+ubcd_add: # ubcd_t ubcd_add(ubcd_t lhs, ubcd_t rhs)
     li a2, 0 # result
     li a3, 0 # shift
     li a4, 0 # carry
-add_1:
+ubcd_add_1:
     andi t0, a0, 0xf
     andi t1, a1, 0xf
     add t0, t0, t1
     add t0, t0, a4
     sltiu t1, t0, 10
-    addi t1, t1, -1 # false mask
+    addi t1, t1, -1
     andi a4, t1, 1
     andi t1, t1, 6
     add t0, t0, t1
     andi t0, t0, 0xf
     sll t0, t0, a3
     or a2, a2, t0
-
     srli a0, a0, 4
     srli a1, a1, 4
     addi a3, a3, 4
     or t0, a0, a1
     or t0, t0, a4
-    bne zero, t0, add_1
-
+    bne zero, t0, ubcd_add_1
     mv a0, a2
+    srli t0, a0, 28
+    bnez t0, ubcd_add_overflow
     ret
+ubcd_add_overflow:
+    error "Overflow occured"
 
-sub:
+ubcd_sub: # ubcd_t ubcd_sub(ubcd_t lhs, ubcd_t rhs)
     li a2, 0 # result
     li a3, 0 # shift
     li a4, 0 # borrow
-sub_1:
+ubcd_sub_1:
     andi t0, a0, 0xf
     andi t1, a1, 0xf
     add t1, t1, a4
     sltu a4, t0, t1
-    neg t2, a4 # true mask
+    neg t2, a4
     andi t2, t2, 10
     add t0, t0, t2
     sub t0, t0, t1
     sll t0, t0, a3
     or a2, a2, t0
-
     srli a0, a0, 4
     srli a1, a1, 4
     addi a3, a3, 4
     or t0, a0, a1
-    bne zero, t0, sub_1
-
+    bne zero, t0, ubcd_sub_1
     mv a0, a2
-    ret
-
-read_number:
-    addi sp, sp, -4
-    sw s1, 0(sp)
-    li s1, 0
-read_number_1:
-    readch
-    xori t1, a0, ' '
-    beq zero, t1, read_number_2
-    slli s1, s1, 4
-    mv s2, ra
-    parse_digit
-    mv ra, s2
-    add s1, s1, a0
-    srai t1, s1, 28
-    andi t1, t1, 0xF
-    beq zero, t1, read_number_1
-read_number_2:
-    mv a0, s1
-    lw s1, 0(sp)
-    addi sp, sp, 4
-    ret
-
-print_number:
-    addi sp, sp, -4
-    sw s1, 0(sp)
-    sw s2, 4(sp)
-    mv s1, sp
-    mv s2, a0
-print_number_1:
-    andi a0, s2, 0xF
-    to_digit
-    addi sp, sp, -1
-    sb a0, 0(sp)
-    srli s2, s2, 4
-    bne zero, s2, print_number_1
-print_number_2:
-    lb a0, 0(sp)
-    addi sp, sp, +1
-    printch
-    bne sp, s1, print_number_2
-
-    lw s1, 0(sp)
-    lw s2, 4(sp)
-    addi sp, sp, +4
     ret
